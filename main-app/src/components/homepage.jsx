af1e3e8a6a424fe49a983570cca692a450df6a4c
kennyg37 <k.ganza@alustudent.com>
2024-01-25 09:51:41 +0200

    app initiation

---
+++ main-app/src/components/homepage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const homepage = () => {
+  return (
+    <>
+        <div className='nav-head'>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+
+    </>
+  )
+}
+
+export default homepage
