bf71b88083667b44d6bafe69d9c99def0ac098d9
tuyishimejohnson <j.tuyishime4@alustudent.com>
2024-01-26 18:41:35 +0200

    Adding ratings.

--- ivuuze-hub/scripts/doctors-profile.js
+++ ivuuze-hub/scripts/doctors-profile.js
@@ -1,11 +1,11 @@
 
-let names = {
-    name: "Johnson Tuyishime",
-    phoneNumber: "+2507888888"
-}
 document.getElementById("my-connect").addEventListener("click", myProfile);
 function myProfile() {
-    document.getElementById("p").innerHTML = `Name:${names.name} \n Phone:${names.phoneNumber}`;
+    let firstNames = {
+        name: "Johnson Tuyishime",
+        phoneNumber: "+2507888888"
+    }
+    document.getElementById("p").innerHTML = `Name:${firstNames.name} \n Phone:${firstNames.phoneNumber}`;
 }
 
 
